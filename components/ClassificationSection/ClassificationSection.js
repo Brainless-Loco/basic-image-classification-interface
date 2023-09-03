6176939fca50798e5122c23693fe0abb1bd74844
Brainless-Loco <dev.tcdas@gmail.com>
2023-09-03 08:16:06 +0600

    Temporary without upload btn

--- components/ClassificationSection/ClassificationSection.js
+++ components/ClassificationSection/ClassificationSection.js
@@ -106,9 +106,9 @@ export default function ClassificationSection() {
     return (
         <Box className='section center'>
             <Box sx={{height:'auto',width:'400px',position:'relative',textAlign:'center'}}>
-                <Button className='uploadBtn' component="label">
+                <Button disabled className='uploadBtn' component="label">
                     <input onChange={handleImageUpload} hidden accept="image/*" multiple type="file" />
-                    **Click on the Image for new selection**
+                    {/* **Click on the Image for new selection** */}
                 </Button>
                 <Image
                     className='image'
